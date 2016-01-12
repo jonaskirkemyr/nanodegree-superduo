6fe52aa834ee6654aa498c51c1d5a0eaae7ab3bc
Jonas Kirkemyr <jonas@kirkemyr.no>
2016-01-12 12:09:34 +0100

    added abstract class to be used by main activity for retrieving title and what navigation to hold as active on fragment change

---
+++ Alexandria3/app/src/main/java/jonask/nano/alexandria3/FragmentTitle.java
@@ -0,0 +1,14 @@
+package jonask.nano.alexandria3;
+
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Jonas on 08.01.2016.
+ */
+public abstract class FragmentTitle extends Fragment
+{
+    public abstract int getTitleResource();
+    public abstract int getNavigationResource();
+
+}
