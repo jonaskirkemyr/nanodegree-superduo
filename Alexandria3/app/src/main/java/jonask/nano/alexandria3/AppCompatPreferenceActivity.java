ac6d5f458c360addd8ee23520fd656ba8f029c68
Jonas Kirkemyr <jonas@kirkemyr.no>
2016-01-12 12:24:26 +0100

    added settings activity. created basically from android studio template

---
+++ Alexandria3/app/src/main/java/jonask/nano/alexandria3/AppCompatPreferenceActivity.java
@@ -0,0 +1,143 @@
+package jonask.nano.alexandria3;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Jonas on 11.01.2016. Created from Settings template A {@link android.preference.PreferenceActivity} which
+ * implements and proxies the necessary calls to be used with AppCompat.
+ */
+public abstract class AppCompatPreferenceActivity extends PreferenceActivity
+{
+    private AppCompatDelegate mDelegate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        getDelegate().installViewFactory();
+        getDelegate().onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        getDelegate().onStop();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        getDelegate().onDestroy();
+    }
+
+    private AppCompatDelegate getDelegate()
+    {
+        if (mDelegate == null)
+        {
+            mDelegate = AppCompatDelegate.create(this, null);
+        }
+        return mDelegate;
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState)
+    {
+        super.onPostCreate(savedInstanceState);
+        getDelegate().onPostCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onPostResume()
+    {
+        super.onPostResume();
+        getDelegate().onPostResume();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        getDelegate().onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResID)
+    {
+        getDelegate().setContentView(layoutResID);
+    }
+
+    @Override
+    public void setContentView(View view)
+    {
+        getDelegate().setContentView(view);
+    }
+
+    @Override
+    public void setContentView(View view, ViewGroup.LayoutParams params)
+    {
+        getDelegate().setContentView(view, params);
+    }
+
+    @Override
+    public void addContentView(View view, ViewGroup.LayoutParams params)
+    {
+        getDelegate().addContentView(view, params);
+    }
+
+    public void invalidateOptionsMenu()
+    {
+        getDelegate().invalidateOptionsMenu();
+    }
+
+    @Override
+    public MenuInflater getMenuInflater()
+    {
+        return getDelegate().getMenuInflater();
+    }
+
+    @Override
+    protected void onTitleChanged(CharSequence title, int color)
+    {
+        super.onTitleChanged(title, color);
+        getDelegate().setTitle(title);
+    }
+
+    public ActionBar getSupportActionBar()
+    {
+        return getDelegate().getSupportActionBar();
+    }
+
+    public void setSupportActionBar(@Nullable Toolbar toolbar)
+    {
+        getDelegate().setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home)
+        {
+           // NavUtils.navigateUpFromSameTask(this);
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+}
